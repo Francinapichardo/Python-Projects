5b824b766d2d21e0fdd4949f78747b8e868b93a8
Francinapichardo <109105885+Francinapichardo@users.noreply.github.com>
2022-10-29 20:39:32 -0400

    class inheritance

--- test9.py
+++ test9.py
@@ -1,32 +1,23 @@
 import sqlite3
 
-conn = sqlite3.connect('test.db')
 
-class User:
-    #Define the attributes of the class
-    name = "Francina"
-    email = "francinap@gmail.com"
-    password = "abcdeg"
-    account_number = 0
+    
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
 
+  def printname(self):
+    print(self.firstname, self.lastname)
 
+class Student(Person):
+    pass
 
-    #Define the methods of the class
-    def login(self):
-        entry_email = input("Enter your email: ")
-        entry_password = input("Enter your password; ")
-        if (entry_email == self.email and entry_password == self.password) :
-            print("Welcome back {}".format(self.name))
-        else:
-            print("You are not authorizided for this page.")
-#Outside od the class you would create an instance of the User class
-new_user = User()
-#Call the login method using the new object
-new_user.login()
+#Use the Person class to create an object, and then execute the printname method:
 
-def __init__(self, name, email, password, account):
-    self.name = name
-    self.email = email
-    self.password = password
-    self.account =account
+x = Person("Katherine", "Linares")
+x.printname()
+
+x = Student("Cristina", "Olsen")
+x.printname()
 
