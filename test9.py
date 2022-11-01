67eba059dda3b42b47001705d59bfeb14f3c9d83
Francinapichardo <109105885+Francinapichardo@users.noreply.github.com>
2022-11-01 00:27:05 -0400

    Inheritance assigment

--- test9.py
+++ test9.py
@@ -1,23 +1,34 @@
 import sqlite3
 
 
+# Initialize first name
     
 class Person:
-  def __init__(self, fname, lname):
-    self.firstname = fname
-    self.lastname = lname
+  def __init__(self,  first_name, last_name= "Katherine",
+               email="katherinel@gmail.com", password = "12345"):
+    self.first_name = first_name
+    self.last_name = last_name
+    self.email = email
+    self.password = 12345
 
   def printname(self):
-    print(self.firstname, self.lastname)
+      print(self.first_name, self.last_name, self.email, self.password )
+
+      
+
+class Grades :
+     print("Check your grades.")
+      
 
 class Student(Person):
     pass
+
+  
+ 
 
 #Use the Person class to create an object, and then execute the printname method:
 
 x = Person("Katherine", "Linares")
 x.printname()
 
-x = Student("Cristina", "Olsen")
-x.printname()
 
