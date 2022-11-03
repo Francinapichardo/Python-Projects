27f8389b08382e82ef03811ecc3a6f0c9b7603d9
Francinapichardo <109105885+Francinapichardo@users.noreply.github.com>
2022-11-03 00:41:21 -0400

    Update

--- test9.py
+++ test9.py
@@ -2,33 +2,40 @@ import sqlite3
 
 
 # Initialize first name
-    
+
 class Person:
-  def __init__(self,  first_name, last_name= "Katherine",
-               email="katherinel@gmail.com", password = "12345"):
-    self.first_name = first_name
-    self.last_name = last_name
-    self.email = email
-    self.password = 12345
+    def __init__(self,  first_name="Katherine", last_name="Linares",
+                 email="katherinel@gmail.com", password="12345"):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = 12345
 
-  def printname(self):
-      print(self.first_name, self.last_name, self.email, self.password )
+    def printname(self):
+        print(self.first_name, self.last_name, self.email, self.password)
 
-      
 
-class Grades :
-     print("Check your grades.")
-      
+class Grades:
+      minimum_passing = 65
+      def __init__(self,score):
+          self.score = score
+          def is_passing(self,score):
+            self.score = score
+            if self.score>=Grade.minimum_passing:
+                return True
+            else:
+                return False
+
+                
 
 class Student(Person):
-    pass
+    def __init__(self, first_name, last_name, year = "2013"):
+        self.graduationyear = 2013
 
-  
- 
 
-#Use the Person class to create an object, and then execute the printname method:
-
-x = Person("Katherine", "Linares")
-x.printname()
-
+# Use the Person class to create an object, and then execute the printname method:
+if __name__ == "__main__":
+    x = Person("Katherine", "Linares")
+    x = Student("Katherine", "Linares, 2013")
+    x.printname()
 
