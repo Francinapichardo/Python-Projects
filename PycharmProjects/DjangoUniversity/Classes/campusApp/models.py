46ec6613fc6db81ebe7185e1cc31297f8f91401d
Francinapichardo <109105885+Francinapichardo@users.noreply.github.com>
2022-11-17 00:27:57 -0500

    updated

---
+++ PycharmProjects/DjangoUniversity/Classes/campusApp/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+# Creates model of University Campus
+class UniversityCampus(models.Model):
+    Campus_name = models.CharField(max_length=60, default="", blank=True, null=False)
+    State = models.IntegerField(max_length=2, default="", blank=True, null=False)
+    Campus_ID = models.CharField(max_length=60, default="", blank=True, null=None)
+
+    # Creates model manager
+    object = models.Manager()
+
+    # Displays the object output values in the form of a string
+    def __str__(self):
+        # Returns the input value of the tittle and instructor name
+        # field as a tuple to display in the browser instead of the default tittles
+        display_course = '{0,tittle}: {0,instructor_name}'
+        return display_course.format(self)
+
+    # Removes added 's' that Django adds to the model name in the browser display
+    class Meta:
+        verbose_name_plural = "University Campus"
